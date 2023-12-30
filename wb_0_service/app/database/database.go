19692bd465cf84d415c840714b2928c720de555f
Andrey <andrej.tsyn@yandex.ru>
2023-12-30 05:07:43 +0300

    feat: add more debug logging

--- wb_0_service/app/database/database.go
+++ wb_0_service/app/database/database.go
@@ -3,9 +3,11 @@ package database
 import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	log "github.com/sirupsen/logrus"
 )
 
 func ConnectToDb(driverName, connStr string) (*sqlx.DB, error) {
+	log.Debugf("DB connection string: %s", connStr)
 	sqlxDb, err := sqlx.Connect(driverName, connStr)
 	if err != nil {
 		return nil, err
--- wb_0_service/app/subscriptions/nats_subs.go
+++ wb_0_service/app/subscriptions/nats_subs.go
@@ -6,6 +6,7 @@ import (
 )
 
 func CreateClient(connectionUrl string, options ...nats.Option) (*nats.Conn, error) {
+	log.Debugf("NATS Connection url: %s; Options: %+v", connectionUrl, options)
 	nc, err := nats.Connect(connectionUrl, options...)
 	if err != nil {
 		log.Error("Can't connect to nats server:")
